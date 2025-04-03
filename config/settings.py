262ab1f156788028f65e4e455597eb375ab3f8f6
Mohammad Khazaei <mkhazaei.dev@gmail.com>
2025-04-03 12:31:40 +0330

    Create 'static' and 'media' files in the BASE_DIR and set their paths in the settings file.

--- config/settings.py
+++ config/settings.py
@@ -116,6 +116,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+MEDIA_URL = "media/"
+
+if DEBUG:
+    STATIC_ROOT = BASE_DIR / "static"
+    MEDIA_ROOT = BASE_DIR / "media"
+else:
+    STATIC_ROOT = ""
+    MEDIA_ROOT = ""
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
